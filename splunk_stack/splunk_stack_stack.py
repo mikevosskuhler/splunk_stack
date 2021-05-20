379549cdd58435bd676b71edcb3a72a178eaf3e7
EC2 Default User <ec2-user@ip-172-31-32-44.us-west-2.compute.internal>
2021-05-20 15:11:17 +0000

    inital commit

--- splunk_stack/splunk_stack_stack.py
+++ splunk_stack/splunk_stack_stack.py
@@ -1,4 +1,5 @@
 from aws_cdk import core as cdk
+from aws_cdk import aws_ec2 as ec2
 
 # For consistency with other languages, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
@@ -12,4 +13,9 @@ class SplunkStackStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        vpc = ec2.Vpc(self, 'vpc')
+        instance_type = ec2.InstanceType('t2.micro')
+        ami = ec2.LookupMachineImage(name = 'splunk_AMI_8.2.0_2021*')
+        # instance_type = 1
+        # ami = 2
+        instance = ec2.Instance(self, 'splunk', instance_type = instance_type, machine_image = ami, vpc = vpc)
