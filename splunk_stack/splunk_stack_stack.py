e39b1d8093cc4a6cf79fd77f5460f6c40c8519d0
EC2 Default User <ec2-user@ip-172-31-32-44.us-west-2.compute.internal>
2021-06-18 18:50:54 +0000

    add listener and squash bugs

--- splunk_stack/splunk_stack_stack.py
+++ splunk_stack/splunk_stack_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import core as cdk
 from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_elasticloadbalancingv2 as lb
+from aws_cdk import aws_elasticloadbalancingv2_targets as lbt
 
 # For consistency with other languages, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
@@ -14,12 +15,14 @@ class SplunkStackStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        vpc = ec2.Vpc(self, 'vpc')
+        vpc = ec2.Vpc(self, 'vpc', max_azs=2)
         instance_type = ec2.InstanceType('t2.micro')
         ami = ec2.LookupMachineImage(name = 'splunk_AMI_8.2.0_2021*')
         splunk_sg = ec2.SecurityGroup(self, 'splunk_sg', vpc = vpc)
         # pub_ips = vpc.select_subnets(ec2.SubnetType('PRIVATE'))
         splunk_instance = ec2.Instance(self, 'splunk', instance_type = instance_type, 
                                 machine_image = ami, vpc = vpc, security_group = splunk_sg)
-        alb = lb.ApplicationLoadBalancer(self, 'alb', vpc = vpc)
+        alb = lb.ApplicationLoadBalancer(self, 'alb', vpc = vpc, internet_facing = True)
         splunk_sg.connections.allow_from(alb, ec2.Port.tcp(8000))
+        listener = alb.add_listener("Listener", port=80, open=True)
+        listener.add_targets("splunk", port=8000, targets=[lbt.InstanceTarget(splunk_instance)] )
